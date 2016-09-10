14be08477c26567c0c89ef2b006e2cde8c4bc557
Mark St.Godard <markstgodard@gmail.com>
2016-09-10 12:01:04 -0500

    script to scale up

--- scripts/up.sh
+++ scripts/up.sh
@@ -1,4 +1,4 @@
-for run in {1..1}
+for run in {1..25}
 do
   date
   app=tick$run
@@ -7,4 +7,7 @@ do
   cf set-env $app REGISTRY_HOST "10.255.67.67:8080"
   cf access-allow $app registry --protocol tcp --port 8080
   cf start $app
+
+  echo "Number of apps registered"
+  curl -s registry.toque.c2c.cf-app.com/api/v1/instances | jq '.instances | length'
 done
