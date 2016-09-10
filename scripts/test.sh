e3c2d7bea01a86be473a30123e120c01a2577374
Mark St.Godard <markstgodard@gmail.com>
2016-09-09 22:56:47 -0500

    test script

---
+++ scripts/test.sh
@@ -0,0 +1,10 @@
+for run in {1..20}
+do
+  date
+  app=tick$run
+  echo $app
+  cf push $app --no-start -i 5 -m 128m
+  cf set-env $app REGISTRY_HOST "10.255.67.67:8080"
+  cf access-allow $app registry --protocol tcp --port 8080
+  cf start $app
+done
