92000ff7b7d9dad2091bb9fe481c24791a0a0a8d
Mark St.Godard <markstgodard@gmail.com>
2016-09-11 15:35:16 -0500

    Add peer discovery

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"math/rand"
 	"net"
 	"net/http"
 	"os"
@@ -13,15 +14,26 @@ import (
 	"time"
 )
 
+var random = rand.New(rand.NewSource(time.Now().UnixNano()))
+
 const interval = 10 * time.Second
 
-var ip string
+var ip, host string
+
+type Peer struct {
+	Address string
+}
+
+type Instances struct {
+	ServiceInstances []serviceInstance `json:"instances"`
+}
 
 type serviceInstance struct {
 	ServiceName string          `json:"service_name"`
 	Endpoint    serviceEndpoint `json:"endpoint"`
 	Status      string          `json:"status,omitempty"`
 	TTL         int             `json:"ttl,omitempty"`
+	Tags        []string
 }
 
 type serviceEndpoint struct {
@@ -34,6 +46,7 @@ type serviceEndpoint struct {
 type heartbeat struct {
 	Host     string
 	interval time.Duration
+	Peer     Peer
 	doneChan chan chan struct{}
 }
 
@@ -52,6 +65,7 @@ func (h *heartbeat) Start() {
 		select {
 		case <-ticker.C:
 			h.Send()
+			h.FindPeer()
 		case stopped := <-h.doneChan:
 			ticker.Stop()
 			close(stopped)
@@ -64,6 +78,11 @@ func (h *heartbeat) Send() error {
 	url := fmt.Sprintf("http://%s/api/v1/instances", h.Host)
 	fmt.Println("url:", url)
 
+	tags := []string{}
+	if h.Peer.Address != "" {
+		tags = append(tags, fmt.Sprintf("peer=%s", h.Peer.Address))
+	}
+
 	s := serviceInstance{
 		ServiceName: fmt.Sprintf("%s/%s", os.Getenv("CF_INSTANCE_GUID"), os.Getenv("CF_INSTANCE_INDEX")),
 		Endpoint: serviceEndpoint{
@@ -72,6 +91,7 @@ func (h *heartbeat) Send() error {
 		},
 		Status: "UP",
 		TTL:    20,
+		Tags:   tags,
 	}
 
 	jsonStr, err := json.Marshal(s)
@@ -89,11 +109,43 @@ func (h *heartbeat) Send() error {
 	}
 	defer resp.Body.Close()
 
-	fmt.Println("response Status:", resp.Status)
 	body, _ := ioutil.ReadAll(resp.Body)
 	fmt.Println("response Body:", string(body))
 
 	return nil
+}
+
+func (h *heartbeat) FindPeer() {
+	url := fmt.Sprintf("http://%s/api/v1/instances", host)
+
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Printf("could not talk to %s\n", url)
+		return
+	}
+	defer resp.Body.Close()
+
+	body, _ := ioutil.ReadAll(resp.Body)
+
+	var instances Instances
+	err = json.Unmarshal(body, &instances)
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+	fmt.Printf("%+v", instances)
+
+	// get a random peer (exclude self)
+	total := len(instances.ServiceInstances)
+	randIndices := random.Perm(total)
+	for i := 0; i < total; i++ {
+		randIdx := randIndices[i]
+		otherIP := instances.ServiceInstances[randIdx].Endpoint.Value
+		if !strings.HasPrefix(otherIP, ip) {
+			h.Peer = Peer{
+				Address: otherIP,
+			}
+		}
+	}
 }
 
 func getOverlayAddr() string {
@@ -117,7 +169,7 @@ func main() {
 		port = "8080"
 	}
 
-	host := os.Getenv("REGISTRY_HOST")
+	host = os.Getenv("REGISTRY_HOST")
 	if host == "" {
 		panic("Missing REGISTRY_HOST env variable")
 	}
--- scripts/up.sh
+++ scripts/up.sh
@@ -1,9 +1,9 @@
-for run in {1..25}
+for run in {1..3}
 do
   date
   app=tick$run
   echo $app
-  cf push $app --no-start -i 5 -m 128m
+  cf push $app --no-start -i 2 -m 128m
   cf set-env $app REGISTRY_HOST "10.255.67.67:8080"
   cf access-allow $app registry --protocol tcp --port 8080
   cf start $app
